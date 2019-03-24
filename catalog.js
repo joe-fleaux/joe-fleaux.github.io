ec9f4d3f7bb123483830a0479fbecd0320ec9cb8
joe-fleaux <48878069+joe-fleaux@users.noreply.github.com>
2019-03-24 22:55:21 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -26,6 +26,80 @@
 	so that subsequent javascript can utilize it. Doesn't do anything per se, but sets things up.
 */
 
+function popDiv(e) {
+
+        if (e.classList.contains('popUp') == false) {
+                if (window.innerWidth < 480) {
+                        var elem = e.querySelector('.openButton');
+
+                        if (elem.getAttribute('href') == '')
+                                return;
+
+                        elem.click();
+                        return;
+                }
+
+                var rect = e.getBoundingClientRect();
+
+                var style = 'position: fixed;' +
+                            'top: 0;bottom: 0;left: 0;right: 0;' +
+                            'padding:' +
+                            (rect.top > 0 ? rect.top : 0) + 'px ' +
+                            (window.innerWidth > rect.right ? window.innerWidth - rect.right : 0) + 'px ' +
+                            (window.innerHeight > rect.bottom ? window.innerHeight - rect.bottom : 0) + 'px ' +
+                            (rect.left > 0 ? rect.left : 0) + 'px';
+
+                e.setAttribute('style', style);
+
+                e.parentElement.setAttribute('style', 'height:' + rect.height + 'px');
+                e.querySelector('div').setAttribute('style', '');
+
+                clearTimeout(e.timeout);
+                e.timeout = setTimeout(function () {
+                        e.classList.add('popUp');
+                }, 20);
+                
+                e.classList.add('popUpOpen');
+        } else {
+                e.setAttribute('style', e.getAttribute('style') + '!important;background-Color:transparent;');
+                e.querySelector('div').setAttribute('style', 'box-shadow: rgba(0, 0, 0, 0.4) 0 0!important');
+
+                e.classList.remove('popUpOpen');
+                
+                clearTimeout(e.timeout);
+                e.timeout = setTimeout(function () {
+                        e.setAttribute('style', 'padding: 0');
+                        e.parentElement.setAttribute('style', '');
+                        e.classList.remove('popUp');
+                        e.querySelector('div').setAttribute('style', '');
+                }, 500);
+        }
+}
+
+function cancelClick(ev, e) {
+        if (e.parentElement.classList.contains('popUp') == true) {
+
+                ev = ev || window.event;
+                if (ev.stopPropagation)
+                        ev.stopPropagation();
+                if (ev.cancelBubble != null)
+                        ev.cancelBubble = true;
+
+        }
+}
+
+function closePopUp(e) {
+        while (e.classList.contains('popUp') == false) {
+                if (e.classList.contains('card-content') || e == document.body)
+                        return;
+
+                e = e.parentElement;
+        }
+
+        popDiv(e);
+}
+
+
 this.atjs = {};
 
 (function (atjs) {
@@ -175,81 +249,6 @@ this.atjs = {};
 	*/
 	
 	
-
-               
-function popDiv(e) {
-
-        if (e.classList.contains('popUp') == false) {
-                if (window.innerWidth < 480) {
-                        var elem = e.querySelector('.openButton');
-
-                        if (elem.getAttribute('href') == '')
-                                return;
-
-                        elem.click();
-                        return;
-                }
-
-                var rect = e.getBoundingClientRect();
-
-                var style = 'position: fixed;' +
-                            'top: 0;bottom: 0;left: 0;right: 0;' +
-                            'padding:' +
-                            (rect.top > 0 ? rect.top : 0) + 'px ' +
-                            (window.innerWidth > rect.right ? window.innerWidth - rect.right : 0) + 'px ' +
-                            (window.innerHeight > rect.bottom ? window.innerHeight - rect.bottom : 0) + 'px ' +
-                            (rect.left > 0 ? rect.left : 0) + 'px';
-
-                e.setAttribute('style', style);
-
-                e.parentElement.setAttribute('style', 'height:' + rect.height + 'px');
-                e.querySelector('div').setAttribute('style', '');
-
-                clearTimeout(e.timeout);
-                e.timeout = setTimeout(function () {
-                        e.classList.add('popUp');
-                }, 20);
-                
-                e.classList.add('popUpOpen');
-        } else {
-                e.setAttribute('style', e.getAttribute('style') + '!important;background-Color:transparent;');
-                e.querySelector('div').setAttribute('style', 'box-shadow: rgba(0, 0, 0, 0.4) 0 0!important');
-
-                e.classList.remove('popUpOpen');
-                
-                clearTimeout(e.timeout);
-                e.timeout = setTimeout(function () {
-                        e.setAttribute('style', 'padding: 0');
-                        e.parentElement.setAttribute('style', '');
-                        e.classList.remove('popUp');
-                        e.querySelector('div').setAttribute('style', '');
-                }, 500);
-        }
-}
-
-function cancelClick(ev, e) {
-        if (e.parentElement.classList.contains('popUp') == true) {
-
-                ev = ev || window.event;
-                if (ev.stopPropagation)
-                        ev.stopPropagation();
-                if (ev.cancelBubble != null)
-                        ev.cancelBubble = true;
-
-        }
-}
-
-function closePopUp(e) {
-        while (e.classList.contains('popUp') == false) {
-                if (e.classList.contains('card-content') || e == document.body)
-                        return;
-
-                e = e.parentElement;
-        }
-
-        popDiv(e);
-}
-
 	
 	
 	atjs.start = function (params) {
