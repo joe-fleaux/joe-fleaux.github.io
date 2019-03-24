384f45679168582a878229627ca6af0b94e89f46
joe-fleaux <48878069+joe-fleaux@users.noreply.github.com>
2019-03-24 20:16:06 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -26,42 +26,6 @@
 	Use this to add custom application-specific logic to your awesometable
 */
 
-(function (atjs) {
-
-	// Save the main function in a different variable, to be called later
-	// ahem, "cheap inheritance"
-
-	atjs.parentStart = atjs.start;
-
-	atjs.start = function (params) {
-		atjs.parentStart(params);   // Let awesometable load up as normal
-
-		// Set up things as you need them to be set up, using the params object
-		// You can edit the <script> content to pass information to that params object
-	};
-
-	atjs.parentUpdate = atjs.update;
-
-	atjs.update = function () {
-		atjs.parentUpdate();  // calls the default handler
-
-		
-
-
-
-/* Do whatever else END */                
-                
-                
-	};
-
-	atjs.controllers.didChange = function (id) {
-		// Lets you know if content of the controllers have changed
-		// No need to call the default handler
-	}
-
-
-}(this.atjs));
-
 // Do whatever else you need to do here.
                
 function popDiv(e) {
@@ -136,6 +100,44 @@ function closePopUp(e) {
 
         popDiv(e);
 }
+
+
+(function (atjs) {
+
+	// Save the main function in a different variable, to be called later
+	// ahem, "cheap inheritance"
+
+	atjs.parentStart = atjs.start;
+
+	atjs.start = function (params) {
+		atjs.parentStart(params);   // Let awesometable load up as normal
+
+		// Set up things as you need them to be set up, using the params object
+		// You can edit the <script> content to pass information to that params object
+	};
+
+	atjs.parentUpdate = atjs.update;
+
+	atjs.update = function () {
+		atjs.parentUpdate();  // calls the default handler
+
+		
+
+
+
+/* Do whatever else END */                
+                
+                
+	};
+
+	atjs.controllers.didChange = function (id) {
+		// Lets you know if content of the controllers have changed
+		// No need to call the default handler
+	}
+
+
+}(this.atjs));
+
 
 
 
