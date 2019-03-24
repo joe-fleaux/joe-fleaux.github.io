65e1239e15641bf5172ddec77ef1190f8751c560
joe-fleaux <48878069+joe-fleaux@users.noreply.github.com>
2019-03-24 20:38:36 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -101,6 +101,15 @@ function closePopUp(e) {
         popDiv(e);
 }
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 
 (function (atjs) {
 
