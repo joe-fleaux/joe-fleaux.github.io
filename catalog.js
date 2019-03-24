aee12735e74d6a4dff65c3929e435985b1bcc15a
joe-fleaux <48878069+joe-fleaux@users.noreply.github.com>
2019-03-24 23:20:13 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -218,7 +218,13 @@ this.atjs = {};
 }(this.atjs));
 
 
-$( "#slider" ).slider();
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 function popDiv(e) {
 
